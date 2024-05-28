ec3e2b56843c335027f387c40eb1adc7058c8c70
Ned Stickler <nedstickler@gmail.com>
2024-05-28 18:16:35 +0100

    Fix timer functionality using threading.Timer

--- src/timer.py
+++ src/timer.py
@@ -1,21 +1,31 @@
 
 import threading
-from time import perf_counter
+from time import perf_counter, sleep
 
-# TODO:
-# - perf_counter before threading.Timer
-# - Set off threading.Timer
-# - If paused before (using cancel) then take difference of perf_counter before and after, subtract from time_left
-# - When timer completes, reset time left to length
 
 class Timer:
-    def __init__(self, length: int) -> None:
-        self.length = length
-        self.time_left = length
-
-    def start(self):
-        print("Timer finished!")
-        self.time_left = self.length
+    def __init__(self, time: int = 10) -> None:
+        self.time = time
+        self.time_left = time
     
-    def get_time(self):
+    def _completed(self) -> None:
+        print("Timer finished!")
+        self.time_left = self.time
+    
+    def _update_timer(self) -> None:
+        current_time = perf_counter()
+        self.time_left -= current_time - self._start_time
+        self._start_time = current_time
+        
+    def start(self) -> None:
+        self._start_time = perf_counter()
+        self._delay_timer = threading.Timer(self.time_left, self._completed)
+        self._delay_timer.start()
+
+    def stop(self) -> None:
+        self._delay_timer.cancel()
+        self._update_timer()
+    
+    def get_time_left(self) -> None:
+        self._update_timer()
         return self.time_left
