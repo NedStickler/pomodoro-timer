c6821f830e1118e2fd54ff5c96fc88cec3cbfd50
Ned Stickler <nedstickler@gmail.com>
2024-06-01 16:57:03 +0100

    Finish GUI

--- src/gui.py
+++ src/gui.py
@@ -26,17 +26,36 @@ def continue_timer():
     root.after(1, continue_timer)
 
 def set_work_time():
-    time = int(hours.get())*60**2 + int(minutes.get())*60 + int(seconds.get())
-    timer.set_work(time)
+    if (h := hours.get()) == "":
+        h = 0
+    if (m := minutes.get()) == "":
+        m = 0
+    if (s := seconds.get()) == "":
+        s = 0
+    try:
+        time = int(h)*60**2 + int(m)*60 + int(s)
+        timer.set_work(time)
+    except ValueError:
+        return
 
 def set_break_time():
-    time = int(hours.get())*60**2 + int(minutes.get())*60 + int(seconds.get())
-    timer.set_break(time)
+    if (h := hours.get()) == "":
+        h = 0
+    if (m := minutes.get()) == "":
+        m = 0
+    if (s := seconds.get()) == "":
+        s = 0
+        
+    try:
+        time = int(h)*60**2 + int(m)*60 + int(s)
+        timer.set_break(time)
+    except ValueError:
+        return
 
 if __name__ == "__main__":
     # Root window setup
     root = Tk()
-    root.geometry("240x220")
+    root.geometry("240x210")
     root.title("Pomodoro Timer")
     root.resizable(False, False)
     root.configure(bg="#273746")
@@ -50,24 +69,27 @@ if __name__ == "__main__":
     timer_colour = "#5DADE2"
 
     # Work/break label and timer
-    category = Label(root, text=timer.current.title(), font=("Arial", 18), bg=timer_colour, fg="white")
-    time = Label(root, text=str(datetime.timedelta(seconds=int(timer.get_time_left()))), font=("Arial", 46), bg=timer_colour, fg="white")
+    category = Label(root, text=timer.current.title(), font=("Century Gothic", 18), bg=timer_colour, fg="white")
+    time = Label(root, text=str(datetime.timedelta(seconds=int(timer.get_time_left()))), font=("Century Gothic", 46), bg=timer_colour, fg="white")
 
     # Timer buttons
-    start = Button(timer_buttons_frame, text="Start", command=timer.start, relief="flat", bg="#273746", fg="white")
-    stop = Button(timer_buttons_frame, text="Stop", command=timer.stop, relief="flat", bg="#273746", fg="white")
-    reset = Button(timer_buttons_frame, text="Reset", command=timer.reset, relief="flat", bg="#273746", fg="white")
+    start = Button(timer_buttons_frame, text="Start", command=timer.start, relief="flat", bg="#273746", fg="white", font=("Century Gothic", 16))
+    stop = Button(timer_buttons_frame, text="Stop", command=timer.stop, relief="flat", bg="#273746", fg="white", font=("Century Gothic", 16))
+    reset = Button(timer_buttons_frame, text="Reset", command=timer.reset, relief="flat", bg="#273746", fg="white", font=("Century Gothic", 16))
     start.place(width=80, height=40)
     stop.place(width=80, height=40, x=80, y=0)
     reset.place(width=80, height=40, x=160, y=0)
 
     # Input boxes
-    hours = Spinbox(input_boxes_frame, from_=0, to=99, increment=1, wrap=True, bg="#2C3E50", buttonbackground="#273746", relief="flat", buttondownrelief="flat", buttonuprelief="flat", fg="white", font=("Arial", 22))
-    minutes = Spinbox(input_boxes_frame, from_=0, to=59, increment=1, wrap=True, bg="#2C3E50", buttonbackground="#273746", relief="flat", buttondownrelief="flat", buttonuprelief="flat", fg="white", font=("Arial", 22))
-    seconds = Spinbox(input_boxes_frame, from_=1, to=59, increment=1, wrap=True, bg="#2C3E50", buttonbackground="#273746", relief="flat", buttondownrelief="flat", buttonuprelief="flat", fg="white", font=("Arial", 22))
-    hours_label = Label(input_boxes_frame, text="h", bg="#273746", fg="white", font=("Arial", 14))
-    minutes_label = Label(input_boxes_frame, text="m", bg="#273746", fg="white", font=("Arial", 14))
-    seconds_label = Label(input_boxes_frame, text="s", bg="#273746", fg="white", font=("Arial", 14))
+    hours = Entry(input_boxes_frame, bg="#2C3E50", relief="flat", fg="white", font=("Century Gothic", 22), justify="center", insertbackground="white")
+    minutes = Entry(input_boxes_frame, bg="#2C3E50", relief="flat", fg="white", font=("Century Gothic", 22), justify="center", insertbackground="white")
+    seconds = Entry(input_boxes_frame, bg="#2C3E50", relief="flat", fg="white", font=("Century Gothic", 22), justify="center", insertbackground="white")
+    hours.insert(0, "0")
+    minutes.insert(0, "25")
+    seconds.insert(0, "0")
+    hours_label = Label(input_boxes_frame, text="h", bg="#2C3E50", fg="white", font=("Century Gothic", 14))
+    minutes_label = Label(input_boxes_frame, text="m", bg="#2C3E50", fg="white", font=("Century Gothic", 14))
+    seconds_label = Label(input_boxes_frame, text="s", bg="#2C3E50", fg="white", font=("Century Gothic", 14))
     hours.place(width=60, height=40, x=0, y=0)
     hours_label.place(width=20, height=40, x=60, y=0)
     minutes.place(width=60, height=40, x=80, y=0)
@@ -76,17 +98,17 @@ if __name__ == "__main__":
     seconds_label.place(width=20, height=40, x=220, y=0)
 
     # Set timer lengths buttons
-    set_work = Button(set_time_frame, text="Set work time", command=set_work_time, relief="flat", bg="#273746", fg="white")
-    set_break = Button(set_time_frame, text="Set break time", command=set_break_time, relief="flat", bg="#273746", fg="white")
+    set_work = Button(set_time_frame, text="Set work", command=set_work_time, relief="flat", bg="#273746", fg="white", font=("Century Gothic", 11))
+    set_break = Button(set_time_frame, text="Set break", command=set_break_time, relief="flat", bg="#273746", fg="white", font=("Century Gothic", 11))
     set_work.place(width=120, height=20, x=0, y=0)
     set_break.place(width=120, height=20, x=120, y=0)
 
     # Place frames
-    category.place(x=0, y=0, width=240, height=40)
-    time.place(x=0, y=40, width=240, height=80)
-    timer_buttons_frame.place(x=0, y=120, width=240, height=40)
-    input_boxes_frame.place(x=0, y=160, width=240, height=40)
-    set_time_frame.place(x=0, y=200, width=240, height=20)
+    category.place(x=0, y=0, width=240, height=30)
+    time.place(x=0, y=30, width=240, height=80)
+    timer_buttons_frame.place(x=0, y=110, width=240, height=40)
+    input_boxes_frame.place(x=0, y=150, width=240, height=40)
+    set_time_frame.place(x=0, y=190, width=240, height=20)
     
     update_category()
     update_time()
